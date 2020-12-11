e646ec3653fa2284c113958ea03fe519cd79d71a
Manasvi1777 <73339478+Manasvi1777@users.noreply.github.com>
2020-12-11 15:56:35 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,32 @@
+class Box{
+
+    constructor(x,y,width,height)
+    {
+    var options={
+        restitution:0.8
+    }
+    this.width=width;
+    this.height=height;
+    this.x=x;
+    this.y=y;
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    World.add(world,this.body);
+
+   }
+
+   display()
+   {
+    var angle=this.body.angle;
+    var pos=this.body.position;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("brown");
+    rect(0,0,this.width,this.height);
+    pop();
+   }
+
+
+
+}
